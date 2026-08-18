0f25e9c2609b1c0c4f4e117bbacf3a9e70113dbd
mohamed abdullah <modyyabdo234@gmail.com>
2026-08-18 14:12:01 +0300

    Prepare for deployment

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,7 +1,7 @@
 import { User }  from "../models/User_model";
 import { Request, Response } from "express";
 import bcrypt from "bcrypt";
-
+import jwt  from "jsonwebtoken";
 export const register = async (req: Request, res: Response) => {
     try {
         const { full_name, email, password, role } = req.body;
@@ -43,3 +43,56 @@ catch(error) {
     });
 }};
 
+export const login = async (req: Request, res: Response) => {
+    try {
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            return res.status(400).json({
+                message: "All fields are required"
+            });
+        }
+
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(401).json({
+                message: "Invalid email or password"
+            });
+        }
+
+        const ismatch = await bcrypt.compare(
+            password,
+            user.password
+        );
+
+        if (!ismatch) {
+            return res.status(401).json({
+                message: "Invalid email or password"
+            });
+        }
+
+        const token = jwt.sign(
+            {
+                id: user._id,
+                role: user.role
+            },
+            process.env.JWT_SECRET!,
+            {
+                expiresIn: "1d"
+            }
+        );
+
+        return res.status(200).json({
+            message: "Login successful",
+            token
+        });
+
+    } catch (error) {
+        console.error("LOGIN ERROR:", error);
+
+        return res.status(500).json({
+            message: "Internal server error"
+        });
+    }
+};
