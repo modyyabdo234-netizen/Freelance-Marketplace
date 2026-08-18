de696e76fdd59b6b5c5405d9b44a4b95241d49da
Jana <janaahmed011206@gmail.com>
2026-08-18 22:18:36 +0300

    ..

--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,18 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes";
+import GigRouter from "./routes/gigs.routes"
+import cookieParser from "cookie-parser"
+
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cookieParser());
 app.use(express.json());
 app.use("/auth", authRoutes);
+app.use("/freelance",GigRouter )
 
 mongoose
     .connect(process.env.MONGO_URI!)
